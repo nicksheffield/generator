521005a4b9c11b2cd545edc4497afec73cc15905
Nick Sheffield <numbereft@gmail.com>
2015-10-13 14:48:19 +1300

    first commit

---
+++ templates/filter.js
@@ -0,0 +1,9 @@
+angular.module('app.filters')
+
+.filter('<%= name %>', function() {
+	return function(input) {
+
+		
+		return input;
+	};
+});
---
+++ templates/resource.js
@@ -0,0 +1,17 @@
+angular.module('app.models')
+
+.factory('<%= capitalizedName %>', function($resource, $config) {
+	var url = '<%= lowerName %>/:id';
+
+	var defaults = {
+		'id': '@id'
+	};
+
+	var methods = {
+		update: {
+			method: 'PUT'
+		}
+	};
+
+	return $resource(url, defaults, methods);
+});
