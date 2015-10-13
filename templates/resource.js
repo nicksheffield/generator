f13dc3355359c869fec23d0e6804aa579c8689a9
Nick Sheffield <numbereft@gmail.com>
2015-10-13 14:49:38 +1300

    added slash to resource url

--- templates/resource.js
+++ templates/resource.js
@@ -1,7 +1,7 @@
 angular.module('app.models')
 
 .factory('<%= capitalizedName %>', function($resource, $config) {
-	var url = '<%= lowerName %>/:id';
+	var url = '/<%= lowerName %>/:id';
 
 	var defaults = {
 		'id': '@id'
