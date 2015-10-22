3ccfd253d7b8cd3e145a785514b04d215cdb3f7a
Nick Sheffield <numbereft@gmail.com>
2015-10-22 14:34:01 +1300

    removed semicolons

--- templates/resource.js
+++ templates/resource.js
@@ -1,17 +1,17 @@
 angular.module('app.resources')
 
 .factory('<%= capitalizedName %>', function($resource) {
-	var url = '/<%= lowerName %>/:id';
+	var url = '/<%= lowerName %>/:id'
 
 	var defaults = {
 		'id': '@id'
-	};
+	}
 
 	var methods = {
 		update: {
 			method: 'PUT'
 		}
-	};
+	}
 
-	return $resource(url, defaults, methods);
+	return $resource(url, defaults, methods)
 });
